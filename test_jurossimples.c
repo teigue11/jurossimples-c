2c9b5f1496c1685aee00f932bee6fa986bc77ac3
teigue11 <dericknjd@gmail.com>
2026-06-02 18:57:24 -0300

    Update test_jurossimples.c

--- test_jurossimples.c
+++ test_jurossimples.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include "function_jurossimples.c"
 
-int calcular_juros(int capital, int taxa, int tempo);
+int calculo_de_juros(int capital, int taxa, int tempo);
 
-int main() {
-    // J = 100 * 0.5 * 10 -> taxa 50
-    printf("Teste 1: %d (Esperado: 50)\n", calcular_juros(100, 50, 10));
-    // J = 100 * 0.25 * 12 -> taxa 25
-    printf("Teste 2: %d (Esperado: 75)\n", calcular_juros(100, 25, 12));
-    // J = 100 * 0.75 * 5 -> taxa 75
-    printf("Teste 3: %d (Esperado: 375)\n", calcular_juros(100, 75, 5));
-    
-    printf("Criado por Derick Ferreira Correa");
+int main()
+{
+
+    printf("Teste 1: %d (Esperado: 50)\n", calculo_de_juros(100, 50, 10));
+
+    printf("Teste 2: %d (Esperado: 75)\n", calculo_de_juros(100, 25, 12));
+
+    printf("Teste 3: %d (Esperado: 375)\n", calculo_de_juros(100, 75, 5));
+
+    printf("Criado por Derick ferreira Correa (https://github.com/TravaChipzzxk)");
+
     return 0;
 }
