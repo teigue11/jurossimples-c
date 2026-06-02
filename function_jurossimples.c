7e786df88bc25fb6664116c2d9ecb7e567aa5905
teigue11 <dericknjd@gmail.com>
2026-06-02 18:55:05 -0300

    Update function_jurossimples.c

--- function_jurossimples.c
+++ function_jurossimples.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int calcular_juros(int capital, int taxa, int tempo) {
-    // dividindo por 100 no final para aplicar a porcentagem
+int calculo_de_juros(int capital, int taxa, int tempo)
+{
+
     int juros = (capital * taxa * tempo) / 100;
     return juros;
 }
