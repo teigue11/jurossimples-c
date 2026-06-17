a8f9e4491aa5fb3ef54f0d51b39d7bc5de677c7c
teigue11 <dericknjd@gmail.com>
2026-06-16 21:19:04 -0300

    Melhorando o arquivo test

--- test_jurossimples.c
+++ test_jurossimples.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+#include <assert.h>
 #include "function_jurossimples.c"
 
 int calculo_de_juros(int capital, int taxa, int tempo);
 
 int main()
 {
+    assert(calculo_de_juros(100, 50, 10) == 500);
+    assert(calculo_de_juros(100, 25, 12) == 300);
+    assert(calculo_de_juros(100, 75, 5) == 375);
 
-    printf("Teste 1: %d (Esperado: 50)\n", calculo_de_juros(100, 50, 10));
-
-    printf("Teste 2: %d (Esperado: 75)\n", calculo_de_juros(100, 25, 12));
-
-    printf("Teste 3: %d (Esperado: 375)\n", calculo_de_juros(100, 75, 5));
-
-    printf("Criado por Derick ferreira Correa");
+    printf("Todos os testes passaram com sucesso!\n");
+    printf("Criado por Derick Ferreira Correa\n");
 
     return 0;
 }
